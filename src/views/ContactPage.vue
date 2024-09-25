4dfe19eec585d25622155c9abc140e8acc572823
Javier Rivera <javierivera29@gmail.com>
2024-09-25 10:07:56 -0600

    Agregado request a Backend para mandar email

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -17,13 +17,14 @@
       <div
         class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md"
       >
-        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+        <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contáctanos</h2>
         <p class="leading-relaxed mb-5 text-gray-600">
-          Post-ironic portland shabby chic echo park, banjo fashion axe
+          Mandanos un mensaje para ponerte en contácto con nosotros
         </p>
         <div class="relative mb-4">
           <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
           <input
+            v-model="email"
             type="email"
             id="email"
             name="email"
@@ -33,6 +34,7 @@
         <div class="relative mb-4">
           <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
           <textarea
+          v-model="message"
             id="message"
             name="message"
             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
@@ -40,8 +42,9 @@
         </div>
         <button
           class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          @click="sendMail"
         >
-          Button
+          Enviar
         </button>
         <p class="text-xs text-gray-500 mt-3">
           Chicharrones blog helvetica normcore iceland tousled brook viral artisan.
@@ -50,3 +53,24 @@
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import { ref } from 'vue';
+
+const email = ref('')
+const message = ref('')
+
+const sendMail = async() => {
+  const body = {
+    email: email.value,
+    message: message.value,
+  }
+  await axios.post('https://pokemon-wiki-backend.onrender.com/api/email', body, ).then((res) => {
+    console.log(res.data)
+  }).catch((err) => {
+    console.log(err);
+    
+  })
+}
+</script>
