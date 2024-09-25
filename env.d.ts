c87ff9b39a05fbbc821e1175f93ee6f4259f7f1b
Javier Rivera <javierivera29@gmail.com>
2024-09-25 12:07:00 -0600

    Acomodado las interfaces y agregado interfaz para variables de entorno

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_FIREBASE_CONFIG: string //Clave de API de Firebase
+  VITE_PKMN_BASEURL: string;  // URL de la API de Pokémon
+  VITE_EMAIL_BASEURL: string;  // URL del backend de email
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
