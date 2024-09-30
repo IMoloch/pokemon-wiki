9f132219543928da378bc1616c3b006126d1ab8b
Javier Rivera <javierivera29@gmail.com>
2024-09-29 19:50:45 -0600

    Agregado SearchBar de PokeInfo

--- src/components/error-component.vue
+++ src/components/error-component.vue
@@ -1,21 +1,7 @@
 <template>
   <div class="text-center">
     <p class="text-3xl font-semibold text-indigo-600">404</p>
-    <h1 class="mt-4 text-3xl font-bold tracking-tight text-[var(--color-heading)] sm:text-5xl">Página no encontrada</h1>
-    <p class="mt-6 text-base leading-7 text-[var(--color-text)]">Lo siento, ha ocurrido un error.</p>
-    <div class="mt-10 flex items-center justify-center gap-x-6">
-      <button
-        @click="routeHome"
-        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        <p class="text-[var(--color-text)]">Regresar al home</p>
-      </button>
-    </div>
+    <h1 class="mt-4 text-3xl font-bold tracking-tight text-[var(--color-heading)] sm:text-5xl">Pokemon no encontrada</h1>
+    <p class="mt-6 text-base leading-7 text-[var(--color-text)]">No se ha encontrado ningun Pokemon con los datos solicitados</p>
   </div>
 </template>
-
-<script setup lang="ts">
-import router from '@/router/router';
-
-const routeHome = () => router.push({ name: 'Home' });
-</script>
